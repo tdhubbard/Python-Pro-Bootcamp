e8cee8bf9490bf9e056697b3b244b7be833074f8
TDHubbard <tyronehubbard@gmail.com>
2025-03-26 19:09:15 -0400

    Update and rename Day1-Band-Name-Generator to Day1-Band-Name-Generator.py

---
+++ Projects/Day1-Band-Name-Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator!")
+meal_name = input("What was the last thing you ate?\n")
+city_name = input("What is a city you would love to visit?\n")
+
+print(f"Your Band Name could be: {} {}! Cool right 🤣")
